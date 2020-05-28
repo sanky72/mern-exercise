c01667b33a2395d6ac9bcb0c6809c2480fe6cf4d
Sankalp Singh <hs12567@gmail.com>
2020-05-28 17:27:23 +0530

    hi

---
+++ backend/models/exercise.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const exerciseSchema = new Schema({
+    username :{type: String, required: true},
+    description: {type: Array, required: true},
+    duration: {type: Number, required: true},
+    date: {type: Date, required: true}
+}, {
+    timestamps: true
+})
+
+const Excercise = mongoose.model('Excercise', exerciseSchema);
+module.exports = Excercise;
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Navbar from "./components/navbar.component";
+import EditExercise from "./components/edit-exercise.component";
+import ExercisesList from "./components/exercises-list.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path = "/" exact component={ExercisesList} />
+      <Route path = "/edit/:id" component={EditExercise}/>
+      <Route path = "/create" component={CreateExercise} />
+      <Route path = "/user" component={CreateUser} />
+    </Router>
+  );
+}
+
+export default App;
